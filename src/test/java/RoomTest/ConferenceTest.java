045e3acde16eea268e943d3b06bbc9d9043f3801
jesus2102 <jesusperezmarin@gmail.com>
2018-06-29 14:43:59 +0100

    conference has capacity passed

--- src/test/java/RoomTest/ConferenceTest.java
+++ src/test/java/RoomTest/ConferenceTest.java
@@ -1,4 +1,24 @@
 package RoomTest;
 
+import Hotel.Guest;
+import Hotel.Room.Conference;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ConferenceTest {
+
+    Conference conference;
+
+    @Before
+    public void before(){
+        conference = new Conference(100,"Embassador", 12000.00);
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(100, conference.getCapacity());
+    }
+
 }
